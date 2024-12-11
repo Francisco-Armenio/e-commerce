769fd35643e5b48925192fb31b0fd2419b583788
francisco <franciscoarmenio1@gmail.com>
2024-12-11 17:45:52 -0300

    se agrego el footer

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
           <Route path="/checkout" element={<Checkout/>}/>
         </Routes>
         
+        <Footer />
         
       </BrowserRouter>
     </CartProvider>
--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <p className="proyecto">Este es un proyecto personal desarrollado por Francisco.</p>
+      <p className="mis-contactos">Conéctate conmigo:</p>
+      <div className="mis-redes">
+        <p className="mi-gmail">
+          <img className="imagen-gmail" src="./imagenes/gmail.png" alt="Logo de Gmail" />
+          franciscoarmenio1@gmail.com
+        </p>
+        <a
+          className="mi-linkedin"
+          href="https://www.linkedin.com/in/francisco-armenio/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="imagen-linkedin" src="./imagenes/linkedin.png" alt="Logo de LinkedIn" />
+          Mi LinkedIn
+        </a>
+        <a
+          className="mi-github"
+          href="https://github.com/francisco-armenio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="imagen-github" src="./imagenes/github.png" alt="Logo de GitHub" />
+          Mi GitHub
+        </a>
+      </div>
+      <p className="derechos">&copy; 2024 Francisco. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
